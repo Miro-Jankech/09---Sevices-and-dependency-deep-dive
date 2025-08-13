bbd7e30c9d043f0e45640059b0d3fe220f028b0d
Miro Jankech <jankechmiro@gmail.com>
2025-08-13 18:50:09 +0200

    defined constant to be injected

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,5 +1,26 @@
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
 
+export const TaskStatusOptions: {
+  value: string,
+  taskStatus: string,
+  text: string
+}[] = [{
+  value: 'open',
+  taskStatus: 'OPEN',
+  text: 'Open'
+},
+{
+  value: 'in-progress',
+  taskStatus: 'IN_PROGRESS',
+  text: 'In-Progress'
+},
+{
+  value: 'done',
+  taskStatus: 'DONE',
+  text: 'Completed'
+},
+]
+
 export interface Task {
   id: string;
   title: string;
